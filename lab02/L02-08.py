d921d869578da56de52c264fcbbbac5632acd439
_MadNess <86233076+jjentaa@users.noreply.github.com>
2024-07-04 23:06:34 +0700

    Update L02-08.py

--- lab02/L02-08.py
+++ lab02/L02-08.py
@@ -1,7 +1,15 @@
 a = int(input())
 b = int(input())
 c = int(input())
-ls = [a, b, c]
-ls.sort()
-for i in ls:
-    print(i, end=' ')
+if(a<b<c):
+    print(a, b, c)
+if(a<c<b):
+    print(a, c, b)
+if(b<a<c):
+    print(b, a, c)
+if(b<c<a):
+    print(b, c, a)
+if(c<a<b):
+    print(c, a, b)
+if(c<b<a):
+    print(c, b, a)
